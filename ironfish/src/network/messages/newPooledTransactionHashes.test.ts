77cb9ffb5089c830708fed19833787ab118d79e6
briannaluna17 <briannaluna3@yahoo.com>
2025-03-31 22:29:49 +0000

    Fix API - removing deprecated function

--- ironfish/src/network/messages/newPooledTransactionHashes.test.ts
+++ ironfish/src/network/messages/newPooledTransactionHashes.test.ts
@@ -8,12 +8,12 @@ import { NewPooledTransactionHashes } from './newPooledTransactionHashes'
 
 describe('PooledTransactionsRequest', () => {
   it('serializes the object into a buffer and deserializes to the original object', () => {
-    const hashes = [...Array(10)].map((_) => blake3(uuid()))
+    let hashes = [...Array(10)].map((_) => blake3(uuid()))
 
-    const message = new NewPooledTransactionHashes(hashes)
+    let message = new NewPooledTransactionHashes(hashes)
 
-    const buffer = serializePayloadToBuffer(message)
-    const deserializedMessage = NewPooledTransactionHashes.deserializePayload(buffer)
+    let buffer = serializePayloadToBuffer(message)
+    let deserializedMessage = NewPooledTransactionHashes.deserializePayload(buffer)
     expect(deserializedMessage).toEqual(message)
   })
 })
